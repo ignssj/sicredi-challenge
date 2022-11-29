46da297bca69842942df526e5b1a4ddf0cca7bbc
ignacio <ign.garcia61@gmail.com>
2022-11-29 17:49:38 -0300

    fix dependencias

---
+++ src/test/java/constants/Endpoints.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class Endpoints {
+    public static final String RESTRICOES_ENDPOINT = "/api/v1/restricoes";
+    public static final String SIMULACOES_ENDPOINT = "/api/v1/simulacoes";
+}
